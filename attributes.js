2f75a819e0628e1b58aff716cd309a4acb2e032c
gtg221h <gtg221h@4ee68c88-d690-764e-8dc5-778bcf9aa04f>
2012-11-20 14:29:50 +0000

    Added attributes.js. Added icons to svn.

---
+++ attributes.js
@@ -0,0 +1,27 @@
+dojo.require("dijit.form.DateTextBox");
+dojo.require("dijit.Toolbar");
+
+dojo.require("dojo.store.Memory");
+
+dojo.require("dojo.dnd.Source");
+dojo.require("dijit.TitlePane");
+
+dojo.require("dijit.layout.BorderContainer"); 
+dojo.require("dijit.layout.ContentPane"); 
+dojo.require("dijit.layout.TabContainer");
+dojo.require("dijit.form.CheckBox");
+
+dojo.require("dojox.layout.ExpandoPane");
+
+dojo.require("esri.utils");
+dojo.require("esri.IdentityManager");
+
+dojo.require("esri.map"); 
+dojo.require("esri.layers.FeatureLayer");
+dojo.require("esri.layers.agsdynamic");
+dojo.require("esri.tasks.gp"); 
+dojo.require("esri.dijit.Legend"); 
+dojo.require("esri.dijit.Popup");
+
+function getAttributesLayer () {
+}
